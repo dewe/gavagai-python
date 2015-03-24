cd05d36d2494f30c5a2e6231c372d680ba9bd14d
Johan Dewe <johan.dewe@gavagai.se>
2015-03-24 22:11:35 +0100

    default host

--- gavagai/client.py
+++ gavagai/client.py
@@ -1,6 +1,8 @@
 class GavagaiClient(object):
 	"""Client for Gavagai Rest API"""
+	
 	def __init__(self, apikey):
 		super(GavagaiClient, self).__init__()
 		self.apikey = apikey
+		self.host = "https://api.gavagai.se"
 		
--- tests/test_client.py
+++ tests/test_client.py
@@ -3,3 +3,7 @@ from gavagai.client import GavagaiClient
 def test_constructor_apikey():
 	client = GavagaiClient("this_is_a_fake_apikey")
 	assert client.apikey == "this_is_a_fake_apikey"
+
+def test_default_host():
+	client = GavagaiClient("fake_apikey")
+	assert client.host == "https://api.gavagai.se"
