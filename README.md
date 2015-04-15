9e878721447593d3ab4704479290f06d12d7327d
Johan Dewe <johan.dewe@gavagai.se>
2015-04-15 20:59:07 +0200

    add requirements to setup.py

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # gavagai-python
-A Gavagai API helper library
+A Gavagai API helper library.
 
 
 ## Set up for development
-
 From root of this repository: 
 
 ```bash
 $ pip install -r requirements.txt
-$ pip install -e .
 ```
--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 setup(
-    name = "gavagai",
-    version = "0.1",
-    packages = find_packages(),
+    name='Gavagai',
+    description='A Gavagai API helper library.',
+    version='0.1',
+    packages=['gavagai'],
+    license='The MIT License (MIT)',
+    install_requires=[
+        'requests>=2.6.0,<3.0',
+        'urllib3==1.10.2',
+        'ndg-httpsclient==0.3.3',
+    ]
 )
