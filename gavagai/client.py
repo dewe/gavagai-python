5c8fc43b7698cf2c5dd386f3774c3519a68b0b03
Johan Dewe <johan.dewe@gavagai.se>
2015-04-15 22:50:50 +0200

    linting w flake8

--- gavagai/client.py
+++ gavagai/client.py
@@ -6,10 +6,12 @@ from exceptions import GavagaiException, GavagaiHttpException
 import urllib3.contrib.pyopenssl
 urllib3.contrib.pyopenssl.inject_into_urllib3()
 
+
 class GavagaiClient(object):
     """Client for Gavagai Rest API"""
-    
-    def __init__(self, apikey=None, host='https://api.gavagai.se', api_version='v3', **kwargs):
+
+    def __init__(self, apikey=None, host='https://api.gavagai.se',
+                 api_version='v3', **kwargs):
         super(GavagaiClient, self).__init__()
         self.apikey = apikey
         self.host = host
@@ -19,8 +21,10 @@ class GavagaiClient(object):
             try:
                 self.apikey = os.environ['GAVAGAI_APIKEY']
             except KeyError:
-                raise GavagaiException('GavagaiClient requires an apikey set explicitly \
-                                        or via the GAVAGAI_APIKEY environment variable')
+                raise GavagaiException(
+                    'GavagaiClient requires an apikey set explicitly \
+                    or via the GAVAGAI_APIKEY environment variable'
+                )
 
     def base_url(self):
         return self.host + '/' + self.api_version
@@ -29,7 +33,9 @@ class GavagaiClient(object):
         path = path.strip('/')
         url = self.base_url() + '/' + path + '?apiKey=' + self.apikey
 
-        res = requests.request(method, url, json=body, allow_redirects=allow_redirects, **self.default_request_options)
+        res = requests.request(method, url, json=body,
+                               allow_redirects=allow_redirects,
+                               **self.default_request_options)
         if res.status_code < 200 or res.status_code > 206:
             message = res.text or 'Unable to complete HTTP request'
             raise GavagaiHttpException(res.status_code, message)
@@ -38,11 +44,11 @@ class GavagaiClient(object):
     def _resource_request(self, resource_name, texts, **kwargs):
         if not isinstance(texts, list):
             raise ValueError('Argument texts is expected to be a list.')
-        body = dict(language='en') 
+        body = dict(language='en')
         body.update(kwargs)
         body['texts'] = ensure_text_objects(texts)
         return self.request(resource_name, 'post', body)
-    
+
     def keywords(self, texts, **kwargs):
         return self._resource_request('/keywords', texts, **kwargs)
 
@@ -54,7 +60,8 @@ class GavagaiClient(object):
 
     def tonality(self, texts, **kwargs):
         response = self._resource_request('/tonality', texts, **kwargs)
-        response.simple_list = types.MethodType(map_text_tonalities, response) # monkey patch this instance
+        # monkey patch this instance
+        response.simple_list = types.MethodType(map_text_tonalities, response)
         return response
 
 
@@ -62,7 +69,7 @@ def ensure_text_objects(texts):
     text_objects = texts[:]
     for i, text in enumerate(text_objects):
         if isinstance(text, unicode) or isinstance(text, basestring):
-            text_objects[i] = { 'id': unicode(i), 'body': unicode(text) }
+            text_objects[i] = {'id': unicode(i), 'body': unicode(text)}
     return text_objects
 
 
@@ -72,10 +79,8 @@ def map_text_tonalities(response):
 
 
 def map_tonality_to_dict(text):
-    scores = lambda x: dict(score=x['score'], normalized_score=x['normalizedScore'])
+    scores = lambda x: dict(score=x['score'],
+                            normalized_score=x['normalizedScore'])
     name = lambda x: x['tone']
-    tonality = {name(t):scores(t) for t in text['tonality']}
+    tonality = {name(t): scores(t) for t in text['tonality']}
     return dict(id=text['id'], tonality=tonality)
-
-
-
