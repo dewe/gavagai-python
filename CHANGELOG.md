5afe13c9a8cc531cc5f4d683a40c9475aed9b921
Johan Dewe <johan.dewe@gavagai.se>
2015-05-10 22:39:03 +0200

    Setup 'changes' release tool

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# [Changelog](https://github.com/dewe/gavagai-python/releases)
+
+## [0.1.0](https://github.com/dewe/gavagai-python/compare/0.1.0...0.1.0)
+
+* Initial release: /keywords, /tonality, /stories and /topics.
+
--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
+import io
+import re
 from setuptools import setup
+
+init_py = io.open('gavagai/__init__.py').read()
+metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", init_py))
+metadata['doc'] = re.findall('"""(.+)"""', init_py)[0]
+
 setup(
     name='Gavagai',
     packages=['gavagai'],
-    version='0.1',
-    description='A Gavagai API helper library.',
-    author = 'Johan Dewe',
-    author_email = 'johan@dewe.net',
-    url = 'https://github.com/dewe/gavagai-python',
+    version=metadata['version'],
+    description=metadata['doc'],
+    author=metadata['author'],
+    author_email=metadata['email'],
+    url=metadata['url'],
     keywords = ['text-analysis', 'api', 'nlp'], 
     license='The MIT License (MIT)',
     install_requires=[
-        'requests>=2.6.0,<3.0',
+        'requests>=2.7.0,<3.0',
         'urllib3',
         'ndg-httpsclient',
     ]
